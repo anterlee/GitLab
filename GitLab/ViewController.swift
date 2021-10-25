bf6a67982335cf435c9b9be898f40d38e4d953f9
anterlee <anter@nemoplay.co.kr>
2021-10-25 15:18:50 +0900

    add main3
    (cherry picked from commit aa417b04d570f58339bbc899560cf44a8cb3d199)

--- GitLab/ViewController.swift
+++ GitLab/ViewController.swift
@@ -15,6 +15,23 @@ class ViewController: NSViewController {
         // Do any additional setup after loading the view.
     }
     
+    func main3(){
+        print("new main3")
+    }
+    
+    func main2(){
+        print("new main")
+    }
+    
+    func devHello3(){
+        print("dev3 hello")
+    }
+
+    
+    func sayHello()  {
+        print("Hello")
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
