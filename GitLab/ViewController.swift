f499bd3ed644527bc9002bbc61120a736d752a8b
anterlee <anter@nemoplay.co.kr>
2021-10-25 14:23:36 +0900

    Initial Commit

---
+++ GitLab/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitLab
+//
+//  Created by anterlee on 25/10/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
